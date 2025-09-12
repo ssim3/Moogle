fe7f02329ff1d870091ed6179051bec0b54c6c2a
Sim Chee Ping <SimCheePing@MacBook-Pro-3.local>
2025-09-13 00:20:59 +0700

    Setup

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
-import asyncio
-import telegram
+import logging
+from telegram import Update
+from telegram.ext import filters, ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler
 from dotenv import load_dotenv
 import os
 
-load_dotenv(override=True)
+load_dotenv()
 BOT_API_KEY = os.getenv("BOT_API_KEY")
 
-async def main():
-    print(BOT_API_KEY)
-    bot = telegram.Bot(BOT_API_KEY)
-    async with bot:
-        updates = (await bot.get_updates())[0]
-        print(updates)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
-if __name__ == "__main__":
-    asyncio.run(main())
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+if __name__ == '__main__':
+
+    application = ApplicationBuilder().token(BOT_API_KEY).build()
+    
+    start_handler = CommandHandler('start', start)
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    
+    application.add_handler(start_handler)
+    application.add_handler(echo_handler)
+    
+    application.run_polling()
